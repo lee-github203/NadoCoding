66fdb62637057e33dbc7c57e0fe57d3792164d77
lee-github203 <lee-github203>
2021-04-30 20:24:30 +0900

    초기 커밋.

---
+++ NadiCoding/loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//printf("Hello World\n");
+
+	// ++
+	/*int a = 10;
+	printf("a는 %d\n", a);
+	a++;
+	printf("a는 %d\n", a);
+	a++;
+	printf("a는 %d\n", a);*/
+
+	int b = 20;
+	printf("b는 %d\n", ++b);
+	printf("b는 %d\n", b++);
+	printf("b는 %d\n", b);
+	return 0;
+}
